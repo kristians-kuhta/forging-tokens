08f735cb882291301a24542b16f37646027fb806
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-04-04 15:22:07 +0300

    Allow metamask connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { ethers } from "ethers";
 
 function App() {
+  const [wallet, setWallet] = useState({});
+
+  async function connectAccount() {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts"
+        });
+        const address = accounts[0];
+        const provider = new ethers.BrowserProvider(window.ethereum)
+        const weiBalance = await provider.getBalance(address);
+        const balance = ethers.formatEther(weiBalance);
+
+        setWallet({address, balance});
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      alert("Please, use a browser that supports Metamask!");
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { wallet.address ?
+          <p>Connected to { wallet.address } ({wallet.balance} MATIC)</p>
+          :
+          <button onClick={connectAccount} >Connect</button>
+        }
       </header>
     </div>
   );
