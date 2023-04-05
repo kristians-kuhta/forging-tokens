714bd04d09d837ff741cffa5235cb71409d433b0
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-04-05 22:54:48 +0300

    Write ABIs and addresses to files for frontend usage

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,24 +4,59 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
+const path = require('path');
+const fs = require('fs');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [owner] = await hre.ethers.getSigners();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const factory = await hre.ethers.getContractFactory('Item');
+  const token = await factory.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const item = await token.deployed();
 
-  await lock.deployed();
+  console.log(`Item deployed to ${token.address}`);
+  const forgeAddress = await item.forge();
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  await saveFrontendFiles(item.address, forgeAddress);
+}
+
+function removePreviousFiles(contractsDir) {
+  fs.unlinkSync(`${contractsDir}/Forge.json`);
+  fs.unlinkSync(`${contractsDir}/Item.json`);
+  fs.unlinkSync(`${contractsDir}/contract-address.json`);
+
+  console.log(`Cleaned out ${contractsDir}`);
+}
+
+function saveFrontendFiles(itemAddress, forgeAddress) {
+  const contractsDir = path.join(__dirname, '/../frontend/src/contracts');
+  removePreviousFiles(contractsDir);
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  const contractAddressesPath = contractsDir + '/contract-address.json';
+
+  fs.writeFileSync(
+    contractAddressesPath,
+    JSON.stringify({ Item: itemAddress, Forge: forgeAddress }, null, 2)
   );
+  console.log(`Addresses written to ${contractAddressesPath}\n`);
+
+  const forgeAbiPath = `${contractsDir}/Forge.json`;
+  const ForgeArtifact = artifacts.readArtifactSync('Forge');
+
+  fs.writeFileSync(forgeAbiPath, JSON.stringify(ForgeArtifact, null, 2));
+  console.log(`Forge abi written to ${forgeAbiPath}`);
+
+  const itemAbiPath = `${contractsDir}/Item.json`;
+  const ItemArtifact = artifacts.readArtifactSync('Item');
+
+  fs.writeFileSync(itemAbiPath, JSON.stringify(ItemArtifact, null, 2));
+  console.log(`Forge abi written to ${itemAbiPath}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
