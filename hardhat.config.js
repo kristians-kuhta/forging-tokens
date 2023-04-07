564a4180f2961ab65414bbef5dab88f44e311a0d
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-04-07 12:36:28 +0300

    Switch hardhat node to use interval mining

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      mining: {
+        auto: false,
+        interval: 5000
+      }
+    }
+  }
 };
