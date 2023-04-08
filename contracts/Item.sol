d9348545192f538cccf22f2c8faa6cefc876a418
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-04-08 14:31:25 +0300

    Remove unused import

--- contracts/Item.sol
+++ contracts/Item.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-// import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import './Forge.sol';
 
