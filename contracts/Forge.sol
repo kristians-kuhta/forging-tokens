9dd607f933c606f127ae13011710289d5bec96ac
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-04-11 16:11:38 +0300

    Format contracts with prettier

--- contracts/Forge.sol
+++ contracts/Forge.sol
@@ -5,123 +5,133 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "./Item.sol";
 
 contract Forge {
-  uint256 public constant MINT_COOLDOWN = 1 minutes;
+    uint256 public constant MINT_COOLDOWN = 1 minutes;
 
-  error TokenIdCannotBeMinted();
-  error TokenCannotBeBurned();
-  error TokenCannotBeTraded();
-  error AddressOnMintCooldown(uint256 remainingSeconds);
+    error TokenIdCannotBeMinted();
+    error TokenCannotBeBurned();
+    error TokenCannotBeTraded();
+    error AddressOnMintCooldown(uint256 remainingSeconds);
 
-  mapping(address => uint256) public lastMint;
+    mapping(address => uint256) public lastMint;
 
-  Item public immutable item;
+    Item public immutable item;
 
-  constructor(address _item) {
-    item = Item(_item);
-  }
-
-  //
-  // Public functions
-  //
-
-  function mint(uint256 _tokenId) public {
-    _checkTokenCanBeMinted(_tokenId);
-    _checkAddressOnMintCooldown();
-    lastMint[msg.sender] = block.timestamp;
-    item.mint(_tokenId, msg.sender);
-  }
-
-  function forge(uint256 _tokenId) public
-  {
-    _forge(_tokenId);
-  }
-
-  function burn(uint256 _tokenId) public {
-    _checkTokenCanBeBurned(_tokenId);
-    item.burn(msg.sender, _tokenId, 1);
-  }
-
-  function trade(uint256 _fromTokenId, uint256 _toTokenId) public {
-    require(_fromTokenId != _toTokenId);
-
-    if (_fromTokenId > 2) {
-      item.burn(msg.sender, _fromTokenId, 1);
-    } else if (_toTokenId < 3) {
-      item.burn(msg.sender, _fromTokenId, 1);
-      item.mint(_toTokenId, msg.sender);
-    } else {
-     revert();
+    constructor(address _item) {
+        item = Item(_item);
     }
-  }
 
-  function mintCooldown() public view returns (bool) {
-    return _addressOnMintCooldown(msg.sender);
-  }
-  //
-  //  Internal functions
-  //
+    //
+    // Public functions
+    //
 
-  function _canTradeToken(uint256 _tokenId) internal view returns (bool) {
-    return _tokenId < 3 && item.balanceOf(msg.sender, _tokenId) > 0;
-  }
-
-  function _addressOnMintCooldown(address _account) internal view returns (bool) {
-    return lastMint[_account] != 0 && lastMint[_account] > block.timestamp - MINT_COOLDOWN;
-  }
-
-  function _checkTokenCanBeMinted(uint256 _tokenId) internal pure {
-    if (_tokenId > 2) {
-      revert TokenIdCannotBeMinted();
+    function mint(uint256 _tokenId) public {
+        _checkTokenCanBeMinted(_tokenId);
+        _checkAddressOnMintCooldown();
+        lastMint[msg.sender] = block.timestamp;
+        item.mint(_tokenId, msg.sender);
     }
-  }
 
-  function _checkTokenCanBeTraded(uint256 _tokenId) internal view {
-    if (!_canTradeToken(_tokenId)) {
-      revert TokenCannotBeTraded();
+    function forge(uint256 _tokenId) public {
+        _forge(_tokenId);
     }
-  }
 
-  function _checkTokenCanBeBurned(uint256 _tokenId) internal pure {
-    if (_tokenId < 4) {
-      revert TokenCannotBeBurned();
+    function burn(uint256 _tokenId) public {
+        _checkTokenCanBeBurned(_tokenId);
+        item.burn(msg.sender, _tokenId, 1);
     }
-  }
 
-  function _checkAddressOnMintCooldown() internal view {
-    if (_addressOnMintCooldown(msg.sender)) {
-      revert AddressOnMintCooldown((lastMint[msg.sender] + MINT_COOLDOWN) - block.timestamp);
+    function trade(uint256 _fromTokenId, uint256 _toTokenId) public {
+        require(_fromTokenId != _toTokenId);
+
+        if (_fromTokenId > 2) {
+            item.burn(msg.sender, _fromTokenId, 1);
+        } else if (_toTokenId < 3) {
+            item.burn(msg.sender, _fromTokenId, 1);
+            item.mint(_toTokenId, msg.sender);
+        } else {
+            revert();
+        }
     }
-  }
 
-  function _forge(uint256 _tokenId) internal {
-    if (_tokenId == 3) {
-      _mintOneBurnTwo(_tokenId, 0, 1);
-    } else if (_tokenId == 4) {
-      _mintOneBurnTwo(_tokenId, 1, 2);
-    } else if (_tokenId == 5) {
-      _mintOneBurnTwo(_tokenId, 0, 2);
-    } else if (_tokenId == 6) {
-      _mintOneBurnThree(_tokenId, 0, 1, 2);
-    } else {
-      revert();
+    function mintCooldown() public view returns (bool) {
+        return _addressOnMintCooldown(msg.sender);
     }
-  }
 
-  function _mintOneBurnTwo(uint256 _mintedTokenId, uint256 _burnedTokenId1, uint256 _burnedTokenId2) internal {
-    item.mint(_mintedTokenId, msg.sender);
-    item.burn(msg.sender, _burnedTokenId1, 1);
-    item.burn(msg.sender, _burnedTokenId2, 1);
-  }
+    //
+    //  Internal functions
+    //
 
-  function _mintOneBurnThree(
-    uint256 _mintedTokenId,
-    uint256 _burnedTokenId1,
-    uint256 _burnedTokenId2,
-    uint256 _burnedTokenId3
-  ) internal {
-    item.mint(_mintedTokenId, msg.sender);
-    item.burn(msg.sender, _burnedTokenId1, 1);
-    item.burn(msg.sender, _burnedTokenId2, 1);
-    item.burn(msg.sender, _burnedTokenId3, 1);
-  }
+    function _canTradeToken(uint256 _tokenId) internal view returns (bool) {
+        return _tokenId < 3 && item.balanceOf(msg.sender, _tokenId) > 0;
+    }
+
+    function _addressOnMintCooldown(
+        address _account
+    ) internal view returns (bool) {
+        return
+            lastMint[_account] != 0 &&
+            lastMint[_account] > block.timestamp - MINT_COOLDOWN;
+    }
+
+    function _checkTokenCanBeMinted(uint256 _tokenId) internal pure {
+        if (_tokenId > 2) {
+            revert TokenIdCannotBeMinted();
+        }
+    }
+
+    function _checkTokenCanBeTraded(uint256 _tokenId) internal view {
+        if (!_canTradeToken(_tokenId)) {
+            revert TokenCannotBeTraded();
+        }
+    }
+
+    function _checkTokenCanBeBurned(uint256 _tokenId) internal pure {
+        if (_tokenId < 4) {
+            revert TokenCannotBeBurned();
+        }
+    }
+
+    function _checkAddressOnMintCooldown() internal view {
+        if (_addressOnMintCooldown(msg.sender)) {
+            revert AddressOnMintCooldown(
+                (lastMint[msg.sender] + MINT_COOLDOWN) - block.timestamp
+            );
+        }
+    }
+
+    function _forge(uint256 _tokenId) internal {
+        if (_tokenId == 3) {
+            _mintOneBurnTwo(_tokenId, 0, 1);
+        } else if (_tokenId == 4) {
+            _mintOneBurnTwo(_tokenId, 1, 2);
+        } else if (_tokenId == 5) {
+            _mintOneBurnTwo(_tokenId, 0, 2);
+        } else if (_tokenId == 6) {
+            _mintOneBurnThree(_tokenId, 0, 1, 2);
+        } else {
+            revert();
+        }
+    }
+
+    function _mintOneBurnTwo(
+        uint256 _mintedTokenId,
+        uint256 _burnedTokenId1,
+        uint256 _burnedTokenId2
+    ) internal {
+        item.mint(_mintedTokenId, msg.sender);
+        item.burn(msg.sender, _burnedTokenId1, 1);
+        item.burn(msg.sender, _burnedTokenId2, 1);
+    }
+
+    function _mintOneBurnThree(
+        uint256 _mintedTokenId,
+        uint256 _burnedTokenId1,
+        uint256 _burnedTokenId2,
+        uint256 _burnedTokenId3
+    ) internal {
+        item.mint(_mintedTokenId, msg.sender);
+        item.burn(msg.sender, _burnedTokenId1, 1);
+        item.burn(msg.sender, _burnedTokenId2, 1);
+        item.burn(msg.sender, _burnedTokenId3, 1);
+    }
 }
--- contracts/Item.sol
+++ contracts/Item.sol
@@ -4,44 +4,48 @@ pragma solidity 0.8.19;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-import './Forge.sol';
+import "./Forge.sol";
 
 contract Item is ERC1155 {
-  error OnlyForgeCanPerformAction();
+    error OnlyForgeCanPerformAction();
 
-  Forge public immutable forge;
+    Forge public immutable forge;
 
-  constructor()
-    ERC1155("ipfs://QmUSuYPXx3nxPaCJcvcNPuEMShzPpuDLGhnDmmtmJn4EJs/{id}") {
-    forge = new Forge(address(this));
-  }
-
-  function contractURI() public pure returns (string memory) {
-    return "ipfs://QmfBi7Gf2M9wLdC41aKKcncS2ns6nVgmffNAF3sFRnubqi";
-  }
-
-  function uri(uint256 _tokenid) override public pure returns (string memory) {
-    return string(
-      abi.encodePacked(
-        "ipfs://QmUSuYPXx3nxPaCJcvcNPuEMShzPpuDLGhnDmmtmJn4EJs/",
-        Strings.toString(_tokenid)
-      )
-    );
-  }
-
-  function _onlyForge() internal view {
-    if (msg.sender != address(forge)) {
-      revert OnlyForgeCanPerformAction();
+    constructor()
+        ERC1155("ipfs://QmUSuYPXx3nxPaCJcvcNPuEMShzPpuDLGhnDmmtmJn4EJs/{id}")
+    {
+        forge = new Forge(address(this));
     }
-  }
 
-  function mint(uint256 _tokenId, address _receiver) external {
-    _onlyForge();
-    _mint(_receiver, _tokenId, 1, "");
-  }
+    function contractURI() public pure returns (string memory) {
+        return "ipfs://QmfBi7Gf2M9wLdC41aKKcncS2ns6nVgmffNAF3sFRnubqi";
+    }
 
-  function burn(address _holder, uint256 _tokenId, uint256 _amount) public {
-    _onlyForge();
-    _burn(_holder, _tokenId, _amount);
-  }
+    function uri(
+        uint256 _tokenid
+    ) public pure override returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "ipfs://QmUSuYPXx3nxPaCJcvcNPuEMShzPpuDLGhnDmmtmJn4EJs/",
+                    Strings.toString(_tokenid)
+                )
+            );
+    }
+
+    function _onlyForge() internal view {
+        if (msg.sender != address(forge)) {
+            revert OnlyForgeCanPerformAction();
+        }
+    }
+
+    function mint(uint256 _tokenId, address _receiver) external {
+        _onlyForge();
+        _mint(_receiver, _tokenId, 1, "");
+    }
+
+    function burn(address _holder, uint256 _tokenId, uint256 _amount) public {
+        _onlyForge();
+        _burn(_holder, _tokenId, _amount);
+    }
 }
