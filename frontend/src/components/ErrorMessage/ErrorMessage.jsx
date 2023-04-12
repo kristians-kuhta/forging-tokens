c6ce50fc28d733d59116912ff61ce94d3b3a8d16
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-04-12 09:54:32 +0300

    Fix ESLint issues for ErrorMessage comp

--- frontend/src/components/ErrorMessage/ErrorMessage.jsx
+++ frontend/src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import Toast from 'react-bootstrap/Toast';
 
-function ErrorMessage({text}) {
-  return <Toast className="d-inline-block m-1" bg="danger" key={Date.now()} >
-    <Toast.Body className={'text-white'}>
-      { text }
-    </Toast.Body>
-  </Toast>;
+function ErrorMessage({ text }) {
+  return (
+    <Toast className="d-inline-block m-1" bg="danger" key={Date.now()}>
+      <Toast.Body className="text-white">
+        { text }
+      </Toast.Body>
+    </Toast>
+  );
 }
+
+ErrorMessage.propTypes = { text: PropTypes.string.isRequired };
 
 export default ErrorMessage;
